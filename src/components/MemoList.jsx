0a358d0a77df7563e7ae1c1c935be6d4a6bcc50b
hinobori <53688542+hinobori@users.noreply.github.com>
2022-01-26 07:57:51 +0900

    アイコン導入

--- src/components/MemoList.jsx
+++ src/components/MemoList.jsx
@@ -1,3 +1,4 @@
+import { Feather } from '@expo/vector-icons';
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
@@ -10,7 +11,7 @@ export default function MemoList() {
                     <Text style={styles.memoListItemDate}>2021年01月12日 10:00</Text>
                 </View>
                 <View>
-                    <Text>X</Text>
+                    <Feather name="x" size={16} color="#B0B0B0" />
                 </View>
             </View>
 
@@ -21,7 +22,7 @@ export default function MemoList() {
                         <Text style={styles.memoListItemDate}>2021年01月12日 10:00</Text>
                     </View>
                     <View>
-                        <Text>X</Text>
+                    <Feather name="x" size={16} color="#B0B0B0" />
                     </View>
                 </View>
             </View>
@@ -33,7 +34,7 @@ export default function MemoList() {
                         <Text style={styles.memoListItemDate}>2021年01月12日 10:00</Text>
                     </View>
                     <View>
-                        <Text>X</Text>
+                    <Feather name="x" size={16} color="#B0B0B0" />
                     </View>
                 </View>
             </View>
